01b9ad8c90562eb53acefd661dfee0e14dd67565
soundbitesibrahim <soundbites2021@gmail.com>
2026-09-12 16:41:41 +0300

    chore: point entry at clean app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppNew';
+import App from './App';
 import './styles.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
